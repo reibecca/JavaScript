a02a6102d350d1859215d8e5816dbaaf5760f06f
becca <rebecca.kaci@gmail.com>
2021-09-27 17:02:40 +0200

    Bonus 1 terminé

---
+++ JS-jour02/bonus1.js
@@ -0,0 +1,12 @@
+// BONUS 1
+
+var min = 1
+var max = 6
+var random = Math.floor (Math.random() * (max-min)) + min
+console.log(random)
+
+if (random == 6) {
+    console.log("Yes I win !")
+} else {
+    console.log("So close...")
+}
