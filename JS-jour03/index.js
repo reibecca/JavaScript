d5696ad72bb208daf6c72677bcf3cef441d2c5ac
becca <rebecca.kaci@gmail.com>
2021-09-28 15:32:00 +0200

    Exercices 1 à 3 du Jour 3 JS terminés

---
+++ JS-jour03/index.js
@@ -0,0 +1,28 @@
+// EXERCICES JS JOUR 3
+
+// 01 - 
+
+var fruits = ["mango", "lemon", "blueberry"]
+console.table(fruits)
+
+
+// 02 - ACCESS
+
+var ingredients = ["eggs", "milk", "butter"]
+console.log(ingredients[2])
+
+
+// 03 - ADD AND REMOVE
+
+var objetcs = ["pen", "book", "lamp"]
+objetcs.unshift("chair")
+console.log(objetcs)
+
+objetcs.pop()
+console.log(objetcs)
+
+objetcs.push("laptop")
+console.log(objetcs)
+
+objetcs.shift()
+console.log(objetcs)
