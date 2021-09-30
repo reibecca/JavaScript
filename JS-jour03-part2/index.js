fcc33926e04414f56eb11c54aedb4951a79001cd
becca <rebecca.kaci@gmail.com>
2021-09-30 15:44:54 +0200

    Correction des exercices 7 à 13

--- JS-jour03-part2/index.js
+++ JS-jour03-part2/index.js
@@ -15,6 +15,7 @@ console.log(sommePair)
 // résultat = 522
 
 
+
 // EXERCICES 2 - NOMBRES IMPAIRS
 
 var sommeImpair = 0
@@ -30,6 +31,7 @@ console.log(sommeImpair)
 // résultat = 83520
 
 console.log("")
+
 
 
 // EXERCICES 3 - PUISSANCE ET RACINE CARREE
@@ -57,6 +59,7 @@ console.log(racineCarré)
 // résultat : [1, 2, 3, 4, 5]
 
 
+
 // EXERCICE 4 - TABLE DE MULTIPLICATION V1
 
 var tableDe3 = 3
@@ -65,6 +68,7 @@ for(var i=1; i <= 10; i++){
     var resultat= tableDe3*i
     console.log(`${tableDe3} x ${i} = ${resultat}`)
 }
+
 
 
 // EXERCICE 5 - TABLE DE MULTIPLICATION V2
@@ -78,6 +82,7 @@ for(var i=1; i <= 10; i++){
 }
 
 
+
 // EXERCICE 6 - FACTORIELLE V1
 
 var resultatSept = 7;
@@ -89,23 +94,36 @@ for(var i = 1; i < 7; i++){
 console.log(`7! = ${resultatSept}`)
 
 
+
 // EXERCICE 7 - FACTORIELLE V2
 
-for(var i=8; i!==0; i--){
-    var resultat=1;
-    //console.log(`i = ${i}`);
-    for (var j=1;j<=i;j++){  
-        resultat*=j;
-        //console.log(`j = ${j}`);     
-    }
-    console.log(`${i}! = ${resultat}`);
+/////// Première technique (compliqué) :
+// for(var i=8; i!==0; i--){
+//     var resultat=1;
+//     console.log(`Résultat factorielle de i = ${i}`);
+//     for (var j=1;j<=i;j++){  
+//         resultat *= j
+//         console.log(`Résultat factorielle de i = ${j}`);     
+//     }
+//     console.log(`${i}! = ${resultat}`)
     
+// }
+
+for (var i = 0; i <= 8; i++){
+    var resultat = 1
+
+    for (var j = 1; j <= i; j++){
+        resultat = resultat * j
+    }
+    console.log(`${i}! = ${resultat}`)
 }
+
 
 
 // EXERCICE 8 - SOMME DES CARRES
 
 var sommeCarre = 0
+
 for (var i = 5; i <= 10; i++){
     var carre = i*i
     console.log(carre)
@@ -115,28 +133,111 @@ console.log(sommeCarre)
 // résultat = 355
 
 
+
 // EXERCICE 9 - COMPTONS
 
 var multiple = 0
 
-for (i = 100; i < 1000; i++){
-    if (i%7 == 0){
+for (var i = 100; i < 1000; i++){
+    if (i%7 === 0){
         console.log(i)
         // affichage de tous les multiples de 7 compris entre 100 et 1000
         multiple += 1
+        // multiple ++ ou multiple = multiple + 1 ------> autre manière d'écrire
     } 
 }
 console.log(multiple)
 // résultat = 128
 
 
+
 // EXERCICE 10 - CHANCEUX 
 
-for (i=0; i<20; i++){
-    if (i > 5){
-        console.log(i)
+var min = 1
+var max = 6
+var sum = 0
+
+for (var i=0; i < 20; i++){
+    var random = Math.floor(Math.random() * (max - min + 1) + min)
+
+    if (random >= 5){
+        console.log(random)
+        sum = sum + random
     }
 }
 
+console.log(sum)
+
+
 
 // EXERCICE 11 - DES BOITES
+
+var box1 = 12;
+var box2 = 5;
+for (var i = 12; i < 14; i++) {
+    box1 = box2;
+    // box1 = 5
+    // box1 = 17
+    console.log(box1 + box2);
+    // box1 + box2 = 10
+    // 17 + 17 = 34
+    box2 = box1 + i;
+    // 17 = 5 + 12
+    // 30 = 17 + 13
+}
+console.log(box1);
+console.log(box2);
+console.log(i);
+// i = 14 
+
+
+
+// EXERCICE 12 - D'AUTRES BOITES
+
+var box1 = 12;
+
+for (var i = 12; i !== 0; i = i - 3) {
+    console.log(box1);
+    // = 12
+    // = 24
+    // = 39
+    box1 = box1 + i;
+    // 24 = 12 + 12
+    // 33 = 24 + 9
+    // 42 = 39 + 3
+}
+
+console.log(box1);
+console.log(i);
+
+
+
+// EXERCICE 13 - ENCORE DES BOITES
+
+var box1 = 0;
+var box2 = 3;
+
+for (var i = 0; i > -4; i--) {
+    if (i % 2 === 0) {
+        console.log(box1 + i);
+        // = 0
+        // = -3
+        box2++;
+        // = 4
+        // = 5
+    } else {
+        console.log(box2 + i);
+        // = 3
+        // = 2
+        box1--;
+        // = -1
+        // = -2
+    }
+}
+
+console.log(box1);
+// = -2
+console.log(box2);
+// = 5
+console.log(i);
+// = -4
