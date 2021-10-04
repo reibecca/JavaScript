cc871024f699572a45c44a9c3718504da3d2234b
becca <rebecca.kaci@gmail.com>
2021-10-04 11:48:53 +0200

    Exercice 2 revision JS terminé

--- JS-révisions/index.js
+++ JS-révisions/index.js
@@ -16,8 +16,11 @@ function sortLetters(str) {
   console.log(sortLetters("konexio"));
 
 
+//////////////
 console.log("")
 console.log("")
+//////////////
+
 
 
 // 02 - XOXO
@@ -31,24 +34,32 @@ function countEach (str){
     var yNumber = 0
 
     for (i = 0; i < str.length; i++){
-        console.log(str)
+        console.log(str.charAt(i))
 
-        var xNumber = xNumber +=i
-        var yNumber = yNumber +=i
-    }
+        if (str.charAt(i)=== "x"){
+            xNumber += 1
+        }
+
+        if (str.charAt(i)=== "o"){
+            yNumber += 1
+        }
+
+     }
 
     if (xNumber === yNumber){
         console.log(true)
-        } else {
+         } else {
         console.log(false)
     }
-}
+ }
 countEach("xxxoooox")
 countEach("xox")
 
 
+//////////////
 console.log("")
 console.log("")
+//////////////
 
 
 
@@ -58,7 +69,129 @@ console.log("")
 // La fonction retourne "Palindrome !" si le mot se lit dans les deux sens, sinon elle retourne "Nope"
 // Vérifiez que vous obtenez un positif avec l'argument "racecar" mais pas avec "laptop"
 
-function checkPal (str){
+ function checkPal (str){
+    var mot = 0
+    var reverseMot = 0
 
-    var = 
+    for (i = 0; i < mot.length; i++){
+        var mot = mot.toString
+        var reverseMot = mot.reverse().join("")
+    }
 }
+
+if (mot === reverseMot){
+    console.log("Palindrome !")
+} else {
+    console.log("Nope")
+}
+
+checkPal("racecar")
+
+// function Palindrome(entree)
+// {
+//     var entree = prompt("racecar").toLowerCase();
+//     var entree_1 = [...entree].reverse().join('');
+// }
+// Palindrome();
+
+// var entree = prompt("racecar").toLowerCase();
+// var entree_1 = [...entree].reverse().join('');
+
+// if (entree === "") {
+//     console.log('racecar');
+// }
+
+// else if (entree === entree_1) {
+//     console.log(entree + ' est un palindrome');
+// }
+
+// else {
+//     console.log(entree + ' n\'est pas un palindrome');
+// }
+
+
+
+
+//////////////
+console.log("")
+console.log("")
+//////////////
+
+
+
+// 04 - SWAP
+
+// Créez une fonction `swap` qui reçoit une string en paramètre
+// La fonction retourne une string avec la casse inverse : une majuscule deviendra minuscule et vice-versa
+// Appelez votre fonction avec l'argument "Hello World" et vérifiez que vous obtenez "hELLO wORLD"
+
+// function swap (str)
+
+
+
+
+//////////////
+console.log("")
+console.log("")
+//////////////
+
+
+
+
+// BONUS 1
+
+// Créez une fonction `makeItSpongeBob` qui reçoit une string en paramètre
+// La fonction retourne une string avec le même texte en alternant majuscule / minuscule
+// Appelez votre fonction avec l'argument "Javascript is easy" et vérifiez le résultat
+
+
+
+//////////////
+console.log("")
+console.log("")
+//////////////
+
+
+
+
+// BONUS 2
+
+// Tester votre exercice 1 avec un mélange de lettres majuscules et minuscules. Est ce que cela fonctionne toujours ?
+// Si non corrigez le !
+
+
+//////////////
+console.log("")
+console.log("")
+//////////////
+
+
+
+
+// BONUS 3
+
+// Que retourne votre fonction de l'exercice 2 si vous lui passez autre chose que des x ou des o ? ou des X et O majuscules ?
+// Rajoutez un message d'erreur explicite dans le premier cas et comptez les majuscules dans le second.
+
+
+
+//////////////
+console.log("")
+console.log("")
+//////////////
+
+
+
+
+// BONUS 4
+
+// Entrainez vous tout seul, il existe de nombreux site avec des exercices pour progressez en JS
+// Si vous êtes plutôt joueur regardez [https://www.codingame.com/](https://www.codingame.com/)
+// Si vous êtes plutôt progression pure et dure, lancez vous dans [https://www.codewars.com/](https://www.codewars.com/)
+// Dans les deux cas, parlez en à votre formatteur pour plus de conseil !
+
+
+//////////////
+console.log("")
+console.log("")
+//////////////
