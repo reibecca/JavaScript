3971f84e1dcadcf85853b1f4ba2d12eea2ceeae3
becca <rebecca.kaci@gmail.com>
2021-10-04 09:56:46 +0200

    Exercices de révision JavaScript

---
+++ JS-révisions/index.js
@@ -0,0 +1,23 @@
+// REVISIONS COURS JAVA SCRIPT 
+
+
+// 01 - ALPHABET 
+
+// Créer une fonction sortLetters qui reçoit une string en paramètre
+// La fonction retourne une string avec les mêmes lettres, dans l'ordre alphabétique
+// Appelez votre fonction avec "konexio" comme argument et vérifiez que vous obtenez "eiknoox"
+
+function sortLetters (string){
+    var wordArray = wordArray.split("")
+    word = wordArray.sort().join("");
+    return word
+}
+sortLetters(konexio)
+console log
+
+
+// function alpha(str) {
+//     var arr = str.split(""); // splits the string
+//     res = arr.sort().join(""); // sort the array and joins to form a string
+//     return res; // returns the result
+//   }
