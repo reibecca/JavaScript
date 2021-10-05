3268090e9715ec34501f96be89792873f5cb3d77
becca <rebecca.kaci@gmail.com>
2021-10-05 11:54:58 +0200

    Mise en page terminée

--- JS-jour05/index.js
+++ JS-jour05/index.js
@@ -2,10 +2,12 @@
 
 console.log(document)
 
-// On cible notre element
+
+// On cible notre element : on affecte une image à l'IA pour la représenter visuellement 
 var image = document.getElementById("ia")
 image.setAttribute("src", "https://i.pinimg.com/564x/5e/9b/24/5e9b24ccbe04a5b11c173929fc09830d.jpg")
 
+// On affecte des images aux éléments "pierre", "papier", "ciseau"
 var imgRock = document.getElementById("rock")
 imgRock.setAttribute("src", "https://farrradise.github.io/PierreFeuilleCiseaux/img/pierre.png")
 
@@ -19,13 +21,37 @@ imgScissor.setAttribute("src", "https://farrradise.github.io/PierreFeuilleCiseau
 
 // On déclare une fonction que l'on va appeler lors du click
 function onButtonRock() {
-    console.log("I clicked on my button")
-  }
+    console.log("I choose rock")
+}
 
-  function onButtonPapper() {
-    console.log("I clicked on my button")
-  }
+function onButtonPapper() {
+    console.log("I choose paper")
+}
 
-  function onButtonCisor() {
-    console.log("I clicked on my button")
-  }
+function onButtonScissor() {
+    console.log("I choose scissor")
+}
+
+
+
+// Faire une fonction qui va choisir aléatoirement le signe de l'IA
+
+var min = 1
+var max = 3
+var choose = 0
+
+function randomIA(num){
+    for (var i=0; i < 3; i++){
+    
+        var random = Math.floor(Math.random() * (max - min + 1) + min)
+
+        if (random >= 2){
+            console.log(random)
+            choose = choose + random
+        }
+    }
+    console.log(i)
+}
+randomIA(2)
+
+
