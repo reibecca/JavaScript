40b36672fc8e6b3a684c9ece7b937a9a4e658244
becca <rebecca.kaci@gmail.com>
2021-10-06 15:24:34 +0200

    exercice 1 terminé

---
+++ JS-jour08/calculate.js
@@ -0,0 +1,23 @@
+// EXERCICES JOUR 8 - NODE, NPM, PROMPT
+
+
+// EXERCICE 01 - CALCULATOR
+
+function calculate (num1, num2, operator){
+    if (operator === '+'){
+        console.log(num1 + num2)
+    }
+    if (operator === '-'){
+        console.log(num1 - num2)
+    }
+    if (operator === '*'){
+        console.log(num1 * num2)
+    }
+    if (operator === '-'){
+        console.log(num1 + num2)
+    }
+    if (operator === '%'){
+        console.log(num1 % num2)
+    }
+}
+calculate(5, 4, '%')
