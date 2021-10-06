8eb55a6da2764cb10986d33ad7cff6f5c379bba5
becca <rebecca.kaci@gmail.com>
2021-10-06 17:30:13 +0200

    Partie 2 de l'excercice 3 terminé

--- JS-jour08/index.js
+++ JS-jour08/index.js
@@ -1,3 +1,41 @@
 // EXERCICES JOUR 8 - NODE, NPM, PROMPT
 
 
+// SUITE EXERCICE 03 
+
+// Créer un fichier index.js et importez les les fonctions de table-utils.js
+
+var operations = require('./table-utils')
+
+
+// Affichez la table d'addition et multiplication en executant le fichier index.jsavec les bons arguments dans le terminal
+
+// dans le terminal : node index.js 5
+console.log(operations)
+
+
+// ---- Résultat dans le terminal ---- 
+
+// 5 x 1 = 5
+// 5 x 2 = 10
+// 5 x 3 = 15
+// 5 x 4 = 20
+// 5 x 5 = 25
+// 5 x 6 = 30
+// 5 x 7 = 35
+// 5 x 8 = 40
+// 5 x 9 = 45
+// 5 x 10 = 50
+
+// 5 + 1 = 6
+// 5 + 2 = 7
+// 5 + 3 = 8
+// 5 + 4 = 9
+// 5 + 5 = 10
+// 5 + 6 = 11
+// 5 + 7 = 12
+// 5 + 8 = 13
+// 5 + 9 = 14
+// 5 + 10 = 15
+
+// { multiply: [Function: multiply], addition: [Function: addition] }
