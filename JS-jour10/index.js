c0e838e9e0637299659fcb8a4a85bb31fdb766da
becca <rebecca.kaci@gmail.com>
2021-10-11 14:58:32 +0200

    Exercice 5 terminé

--- JS-jour10/index.js
+++ JS-jour10/index.js
@@ -100,4 +100,16 @@ var numbers = array.filter(function(number){
 })
 
 console.log(numbers)
+
+
+
+// EXERCICE 05 - FILTER EVEN
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+var even = numbers.filter(function(evenNum){
+    return evenNum % 2 === 0
+})
+
+console.log(even)
  
